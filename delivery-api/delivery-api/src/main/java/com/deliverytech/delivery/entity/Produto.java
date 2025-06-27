31a3281cfe01dc47f3723540558dc7e92d797313
Débora Freire <deborafreiresoares@outlook.com>
2025-06-26 22:41:58 -0300

    Create Produto.java

---
+++ delivery-api/delivery-api/src/main/java/com/deliverytech/delivery/entity/Produto.java
@@ -0,0 +1,88 @@
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Produto<Restaurante> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(length = 255)
+    private String descricao;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal preco;
+
+    @Column(length = 50)
+    private String categoria;
+
+    @Column(nullable = false)
+    private Boolean disponivel = true;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurante_id", referencedColumnName = "id")
+    private Restaurante restaurante;
+
+    // Getters e Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public Restaurante getRestaurante() {
+        return restaurante;
+    }
+
+    public void setRestaurante(Restaurante restaurante) {
+        this.restaurante = restaurante;
+    }
+}
